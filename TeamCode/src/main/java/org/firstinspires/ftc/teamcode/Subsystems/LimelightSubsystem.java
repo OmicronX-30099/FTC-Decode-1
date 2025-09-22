8f54ede9edc5ee949b0123977ccb320659aa3d12
AchintyaAkula <achintya.akula@gmail.com>
2025-09-21 20:08:10 -0700

    Limelight

--- TeamCode/src/main/java/org/firstinspires/ftc/teamcode/Subsystems/LimelightSubsystem.java
+++ TeamCode/src/main/java/org/firstinspires/ftc/teamcode/Subsystems/LimelightSubsystem.java
@@ -1,4 +1,18 @@
 package org.firstinspires.ftc.teamcode.Subsystems;
 
-public class LimelightSubsystem {
+import com.qualcomm.hardware.limelightvision.Limelight3A;
+
+import dev.nextftc.core.subsystems.Subsystem;
+import dev.nextftc.ftc.ActiveOpMode;
+
+public class LimelightSubsystem implements Subsystem {
+    public static final LimelightSubsystem INSTANCE = new LimelightSubsystem()
+    private LimelightSubsystem() { }
+
+    public Limelight3A limelight;
+
+    @Override
+    public void initialize() {
+        limelight = ActiveOpMode.hardwareMap
+    }
 }
