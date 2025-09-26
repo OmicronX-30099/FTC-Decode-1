6cfeff1d129c640cf76ef57af7eac3f1e6ea52e0
AchintyaAkula <achintya.akula@gmail.com>
2025-09-25 19:52:42 -0700

    Setup for Config Constants

--- TeamCode/src/main/java/org/firstinspires/ftc/teamcode/Constants/ConfigConstants.java
+++ TeamCode/src/main/java/org/firstinspires/ftc/teamcode/Constants/ConfigConstants.java
@@ -1,4 +1,16 @@
 package org.firstinspires.ftc.teamcode.Constants;
 
 public class ConfigConstants {
+    // DT Config Constants
+    public static String motor_fl_config = "motor_fl";
+    public static String motor_fr_config = "motor_fr";
+    public static String motor_bl_config = "motor_bl";
+    public static String motor_br_config = "motor_br";
+
+    // Shooter Config Constants
+    public static String motor_fwl_config = "motor_fwl";
+    public static String motor_fwr_config = "motor_fwr";
+    public static String motor_turret_config = "motor_turret";
+    public static String motor_intake_config = "motor_intake";
+    public static String servo_hood_config = "servo_hood";
 }
